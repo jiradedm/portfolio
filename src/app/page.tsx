34ccde92681718245a16ac5586b36d9d4599a910
Jiraded <jiraded27@gmail.com>
2024-03-10 12:51:22 +0700

    change sentence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <div className="h-full flex flex-col gap-1 justify-center items-center font-semibold">
       <div>Hi👋, I&apos;m <span className="text-yellow-200">Jiraded</span>.</div>
-      <div>This is my personal website, but it&apos;s not bein done.</div>
-      <div>Because I&apos;m working on Seven Knights Idle Adventure Damage Calculator.</div>
+      <div>This is my website, but it&apos;s not being done</div>
+      <div>because I&apos;m still working on Seven Knights Idle Adventure Damage Calculator.</div>
       <div>🔥<a className="underline" target="_blank" href="https://sevenknights.kkatana.dev">Check it out !</a>🔥</div>
     </div>
   );
